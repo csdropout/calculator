54c58c52f9833d442b348632722060fe5f6ea039
qile <98936712+csdropout@users.noreply.github.com>
2026-01-30 15:20:24 +0800

    Remove comments

--- script.js
+++ script.js
@@ -1,22 +1,20 @@
-// add
 function add(a, b) {
   return a + b;
 }
-// subtract
+
 function subtract(a, b) {
   return a - b;
 }
-// multiply
+
 function multiply(a, b) {
   return a * b;
 }
-// divide
+
 function divide(a, b) {
   return +(a / b).toFixed(2);
 }
 
 const DIVIDE_BY_0_MSG = "SNEAKY SNEAKY";
-// operate function takes an operator and two numbers and calls the correct operation
 function operate(a, b, operator) {
   a = Number(a);
   b = Number(b);
@@ -41,38 +39,27 @@ function operate(a, b, operator) {
       res = a;
       break;
   }
-  return String(+res.toFixed(5))
+  return String(+res.toFixed(5));
 }
 
-// variables for first number, second number and operator
 let firstNumber = "";
 let secondNumber = "";
 let operator = "";
-
-// any operator pressed afterwards should change the operator
-// boolean flag for when operator is selected, so user updates the second number
 let operatorPressed = false;
+let isFloat = false;
 
-// get display
 const display = document.querySelector("#display");
-
 function updateText() {
   display.textContent = `${firstNumber} ${operator} ${secondNumber}`.trim();
 }
 
-// Create the functions that update one of your number variables when the calculator’s digit buttons are clicked.
-// Your calculator’s display should also update to reflect the value of that number variable.
 const numberButtons = document.querySelectorAll("button.number");
 numberButtons.forEach((button) => {
   button.addEventListener("click", () => {
-    // first number should be before operator is pressed
-    // if operator is pressed
-    // first number should not be updated, any number pressed should update second number
     !operatorPressed
       ? (firstNumber += button.textContent)
       : (secondNumber += button.textContent);
 
-    // update text
     updateText();
   });
 });
@@ -97,37 +84,22 @@ operatorButtons.forEach((button) => {
 
 const clearButton = document.querySelector("#clear");
 clearButton.addEventListener("click", () => {
-  reset()
+  reset();
   display.textContent = "Cleared!";
 });
 
-// equal button should call operate function and display the result
 const equalButton = document.querySelector("#equal");
 equalButton.addEventListener("click", () => {
-  if (firstNumber === '' || secondNumber === '' || operator === '') return;
+  if (firstNumber === "" || secondNumber === "" || operator === "") return;
 
   const res = operate(firstNumber, secondNumber, operator);
   reset();
   display.textContent = res;
 });
 
-// Your calculator should not evaluate more than a single pair of numbers at a time.
-// 12 + 7 entered
-// if user enters another operator example '-'
-// evaluate the first pair of numbers
-
-// if user divides by 0, error message should be display, data should be cleared
-
-// When a result is displayed, pressing a new digit should clear the result and start a new calculation
-// instead of appending the digit to the existing result. Check whether this is the case on your calculator!
-
-// Pressing = before entering all of the numbers or an operator could cause problems!
-
-let isFloat = false;
 const decimalButton = document.querySelector("#decimal");
 decimalButton.addEventListener("click", () => {
   if (!isFloat) {
-    // first number
     !operatorPressed
       ? firstNumber
         ? (firstNumber += ".")
@@ -140,28 +112,22 @@ decimalButton.addEventListener("click", () => {
   }
 });
 
-// reference delete button
-// check in order: second number, operator, first number
-// if decimal point is deleted, set flag to false
-// either use Number.isInteger(Number(num)) or check if deleted character is a '.'
-// first method might result in multiple decimal points
-
-const deleteButton = document.querySelector('#delete')
-deleteButton.addEventListener('click', () => {
-    if (secondNumber) {
-        const lastChar = secondNumber.at(-1)
-        lastChar === '.'? isFloat = false : isFloat = true
-        secondNumber = secondNumber.slice(0, -1)
-    } else if (operator) {
-        operator = ''
-        firstNumber.includes('.')? isFloat = true : isFloat = false
-    } else if(firstNumber) {
-        const lastChar = firstNumber.at(-1)
-        lastChar === '.'? isFloat = false : isFloat = true
-        firstNumber = firstNumber.slice(0, -1)
-    }
-    updateText()
-})
+const deleteButton = document.querySelector("#delete");
+deleteButton.addEventListener("click", () => {
+  if (secondNumber) {
+    const lastChar = secondNumber.at(-1);
+    lastChar === "." ? (isFloat = false) : (isFloat = true);
+    secondNumber = secondNumber.slice(0, -1);
+  } else if (operator) {
+    operator = "";
+    firstNumber.includes(".") ? (isFloat = true) : (isFloat = false);
+  } else if (firstNumber) {
+    const lastChar = firstNumber.at(-1);
+    lastChar === "." ? (isFloat = false) : (isFloat = true);
+    firstNumber = firstNumber.slice(0, -1);
+  }
+  updateText();
+});
 
 function reset() {
   firstNumber = "";
